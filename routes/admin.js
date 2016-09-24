6ca31a3d226c834f22290b49b34d077063002bf5
yura-koderra <yura@koderra.com>
2016-09-24 13:03:01 +0300

    add user page

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,13 @@
-var adminApi = require('../handlers/admin');
+var handler = require('../handlers/admin');
 
 module.exports = function (app) {
-    app.get('/admin', adminApi.main);
-    app.get('/admin/tovs', adminApi.tovs);
-    app.get('/admin/categ', adminApi.categ);
-    app.get('/admin/reqs', adminApi.reqs);
-    app.get('/admin/stocks', adminApi.stocks);
-    app.get('/admin/curr', adminApi.curr);
-    app.get('/admin/deliv', adminApi.deliv);
-    app.get('/admin/com', adminApi.com);
-    app.get('/admin/users', adminApi.listUsers);
+    app.get('/admin', handler.main);
+    app.get('/admin/tovs', handler.tovs);
+    app.get('/admin/categ', handler.categ);
+    app.get('/admin/reqs', handler.reqs);
+    app.get('/admin/stocks', handler.stocks);
+    app.get('/admin/curr', handler.curr);
+    app.get('/admin/deliv', handler.deliv);
+    app.get('/admin/com', handler.com);
+    app.get('/admin/users', handler.listUsers);
 };
