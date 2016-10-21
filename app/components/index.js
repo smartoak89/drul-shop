9163e7708d88a6a3a97b9ba422b1df2dfbec50d8
yura-koderra <yura@koderra.com>
2016-10-21 09:22:31 +0300

    change service

--- app/components/index.js
+++ app/components/index.js
@@ -1,9 +1,7 @@
-
-var app = angular.module('app');
-
-app.component('index', {
-    templateUrl: "components/index.html",
-    controller: ['myf', function(myf) {
-        this.test = myf.blabla
-    }]
-});
+angular.module('app')
+    .component('index', {
+        templateUrl: "components/index.html",
+        controller: ['test', function(test) {
+            this.testS = test;
+        }]
+    });
