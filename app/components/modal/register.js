abf894c98c55d317963d30b5b5e8bb8b79bf8563
yura-koderra <yura@koderra.com>
2016-10-22 17:07:04 +0300

    register

--- app/components/modal/register.js
+++ app/components/modal/register.js
@@ -1,5 +1,26 @@
 angular.module('app')
-    .controller('register', ['$uibModalInstance', '$scope', function ($uibModalInstance, $scope) {
+    .controller('register', ['$uibModalInstance', '$scope', 'Httpquery', function ($uibModalInstance, $scope, Httpquery) {
+        $scope.user = {};
+        $scope.error = null;
+
+        $scope.register = function () {
+          if (isValid() == true) {
+              console.log('isValid')
+          }
+        };
+
+        var reg = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        function isValid () {
+            if (!$scope.user.firstname) return $scope.error = 'Пожалуйста введите имя!';
+            if (!$scope.user.lastname) return $scope.error = 'Пожалуйста введите Фамилию!';
+            if (!$scope.user.email) return $scope.error = 'Пожалуйста введите email!';
+            if (!reg.test($scope.user.email)) return $scope.error = 'Некоректный email!';
+            if (!$scope.user.phone) return $scope.error = 'Пожалуйста введите номер телефона!';
+
+            $scope.error = null;
+            return true;
+        }
+
         $scope.close = function () {
             $uibModalInstance.dismiss();
         }
